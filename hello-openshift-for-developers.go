14cbbe3e59cba468b66d87775755bf9697b2eb6d
Joerg Kleine-Gung <jkgung@web.de>
2024-08-17 10:52:07 +0200

    changed Text

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -10,7 +10,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers!"
+		response = "Hello from "
 		response += " IP Address: " + getCurrentIPAddress()
 	}
 
